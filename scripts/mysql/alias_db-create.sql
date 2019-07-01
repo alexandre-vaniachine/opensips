46e02401523844239209d7777133182f0e529692
Alexandre Vaniachine <avaniachine@thinq.com>
2019-07-01 16:24:53 -0400

    Report errors, remove INSERT

--- scripts/mysql/alias_db-create.sql
+++ scripts/mysql/alias_db-create.sql
@@ -1,4 +1,4 @@
-INSERT INTO version (table_name, table_version) values ('dbaliases','2');
+#INSERT INTO version (table_name, table_version) values ('dbaliases','2');
 CREATE TABLE `dbaliases` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `alias_username` char(64) NOT NULL DEFAULT '',
@@ -7,6 +7,5 @@ CREATE TABLE `dbaliases` (
   `domain` char(64) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `alias_idx` (`alias_username`,`alias_domain`),
-  KEY `target_idx` (`username`, `domain`)
+  KEY `target_idx` (`username`,`domain`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
